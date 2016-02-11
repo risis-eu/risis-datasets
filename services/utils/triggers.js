00af2466bc37ffdd15db616fa8d820b9e7497745
ali1k <hyperir@gmail.com>
2016-02-11 15:34:05 +0100

    minor

--- services/utils/triggers.js
+++ services/utils/triggers.js
@@ -11,7 +11,7 @@ export default {
                 if(el.type !== 'USER'){
                     etext = 'Dear '+ el.firstName +',\n The decision for the visit request application #' + appName + ' has changed by dataset coordinator (' + username +') to "'+newObjectValue+'": \n \n '+ appLink +' \n \n Please sign in to RISIS Datasets Portal with your username ('+el.username+'): \n \n http://datasets.risis.eu/ \n \n and check the applications list for further information. \n \n -- on behalf of RISIS Datasets Portal';
                     //send email
-                    sendMail('applicationDecisionChange', 'datasets@risis.eu', el.mbox, 'RISIS ['+el.type +'] Decision Update for Visit Request Application #' + appName + 'by DSO: ' + newObjectValue, etext, etext);
+                    sendMail('applicationDecisionChange', 'datasets@risis.eu', el.mbox, 'RISIS ['+el.type +'] Decision Update for Visit Request Application #' + appName + ' by DSO: ' + newObjectValue, etext, etext);
                 }
             });
             break;
@@ -22,7 +22,7 @@ export default {
             notifList.forEach(function(el) {
                 if(el.type !== 'USER'){
                     etext = 'Dear '+ el.firstName +',\n The decision for the visit request application #' + appName + ' has changed by PRB (' + username +') to "'+newObjectValue+'": \n \n '+ appLink +' \n \n Please sign in to RISIS Datasets Portal with your username ('+el.username+'): \n \n http://datasets.risis.eu/ \n \n and check the applications list for further information. \n \n -- on behalf of RISIS Datasets Portal';
-                    sendMail('applicationDecisionChange', 'datasets@risis.eu', el.mbox, 'RISIS ['+el.type +'] Decision Update for Visit Request Application #' + appName + 'by PRB: '+ newObjectValue, etext, etext);
+                    sendMail('applicationDecisionChange', 'datasets@risis.eu', el.mbox, 'RISIS ['+el.type +'] Decision Update for Visit Request Application #' + appName + ' by PRB: '+ newObjectValue, etext, etext);
                 }
             });
             break;
@@ -33,7 +33,7 @@ export default {
             notifList.forEach(function(el) {
                 if(el.type !== 'USER'){
                     etext = 'Dear '+ el.firstName +',\n Final decision for visit request application #' + appName + ' decided by "' + username +'" is "'+newObjectValue+'": \n \n '+ appLink +' \n \n Please sign in to RISIS Datasets Portal with your username ('+el.username+'): \n \n http://datasets.risis.eu/ \n \n and check the applications list for further information. \n \n -- on behalf of RISIS Datasets Portal';
-                    sendMail('applicationDecisionChange', 'datasets@risis.eu', el.mbox, 'RISIS ['+el.type +'] Final Decision for Visit Request Application #' + appName + 'by FCB:', etext, etext);
+                    sendMail('applicationDecisionChange', 'datasets@risis.eu', el.mbox, 'RISIS ['+el.type +'] Final Decision for Visit Request Application #' + appName + ' by FCB:', etext, etext);
                 }else{
                     etext = 'Dear '+ el.firstName +',\n Final decision for your visit request application #' + appName + ' is "'+newObjectValue+'": \n \n '+ appLink +' \n \n Please sign in to RISIS Datasets Portal with your username ('+el.username+'): \n \n http://datasets.risis.eu/ \n \n and check the your application details. \n \n -- on behalf of RISIS Datasets Portal';
                     sendMail('applicationDecisionChange', 'datasets@risis.eu', el.mbox, 'RISIS ['+el.type +'] Final Decision for your Visit Request Application #' + appName , etext, etext);
