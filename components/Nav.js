4e300f4bfbd646444d33188de80fb4931b4d1e4d
ali1k <hyperir@gmail.com>
2016-08-15 10:10:57 +0200

    added cookie notifications

--- components/Nav.js
+++ components/Nav.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import {NavLink} from 'fluxible-router';
 import {appFullTitle, appShortTitle, enableAuthentication} from '../configs/general';
+import CookieBanner from 'react-cookie-banner';
+
 
 class Nav extends React.Component {
     componentDidMount(){
@@ -29,6 +31,7 @@ class Nav extends React.Component {
         }
         return (
             <nav ref="defaultNavbar" className="ui black menu inverted navbar page grid page-header">
+                    <CookieBanner message='This website uses cookies to ensure you get the best experience on our website.' cookie='user-has-accepted-cookies' dismissOnScroll={true} />
                     <NavLink routeName="home" className="brand item" activeClass="activei"><img className="ui image" src="/assets/img/risis_logo_full.jpg" alt="RISIS" /></NavLink>
                     <NavLink routeName="home" className="brand item header" activeClass="active"> Datasets Portal</NavLink>
                     <a className="ui item blue label" href="http://sms.risis.eu">SMS Platform</a>
@@ -38,6 +41,7 @@ class Nav extends React.Component {
                     <div className="right menu">
                         {userMenu}
                     </div>
+
             </nav>
         );
     }
